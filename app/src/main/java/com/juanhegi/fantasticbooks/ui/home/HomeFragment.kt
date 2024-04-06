5e7277819de892b24127c186a6a400ec2490a327
Juan Antonio Herraiz <musasime4@gmail.com>
2024-04-06 14:12:29 +0200

    fragmento inicioÃ

--- app/src/main/java/com/juanhegi/fantasticbooks/ui/home/HomeFragment.kt
+++ app/src/main/java/com/juanhegi/fantasticbooks/ui/home/HomeFragment.kt
@@ -1,9 +1,12 @@
 package com.juanhegi.fantasticbooks.ui.home
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -12,10 +15,11 @@ import com.juanhegi.fantasticbooks.databinding.FragmentHomeBinding
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-
+    private lateinit var maps:ImageView
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,10 +31,13 @@ class HomeFragment : Fragment() {
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        maps = binding.maps
+        maps.setOnClickListener{
+            val latitude = 39.46385633926566
+            val longitude = -0.4676711968430236
+            val uri = "https://www.google.com/maps?q=$latitude,$longitude"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(uri))
+            startActivity(intent)
         }
         return root
     }
